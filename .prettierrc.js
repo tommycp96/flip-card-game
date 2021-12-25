1498f46b234fbcd219d85cd53984190963c4ed20
tommycp96 <tommycp8@gmail.com>
2021-12-25 16:12:29 +0700

    project init and dependencies setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  backetSpacing: true,
+  endOfLine: 'lf',
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+};
+module.exports = options;
